97e41e1c8b1582e243b1e8c31929d976d40707a2
alexbrozovich <alexbrozovich@gmail.com>
2018-02-26 00:15:57 -0500

    The program can now play a full game of Uno against itself

--- unoBot.py
+++ unoBot.py
@@ -29,9 +29,13 @@ class player():
 
     def drawCards(self, drawnCards):
         i = 0
+        print self.name + " draws " + str(len(drawnCards)) + " cards"
         while( i < len(drawnCards)):
             self.hand.append(drawnCards[i])
             i += 1
+
+    def playCard(self, numCardToPlay):
+        return self.hand.pop((numCardToPlay))
 
 class unoDeck():
     def __init__(self):
@@ -61,11 +65,18 @@ class unoDeck():
 
     def length(self):
         return len(self.unoDeck)
+    
+    def append(self, card):
+        self.unoDeck.append(card)
         
 class table():
     def __init__(self, deck):
         self.deck = deck
         self.playerList = []
+        self.pile = []
+        self.currentPlayer = 0
+        self.direction = 1
+        self.gameOver = False
     
     def createPlayer(self, playerName):
         self.playerList.append(player(playerName))
@@ -73,7 +84,114 @@ class table():
     def dealOpeners(self):
         for item in self.playerList:
             item.drawCards(self.deck.dealCards(7))
-        
+    
+    def playerPlayCard(self, playerNum, cardNum):
+        colors = ["Red", "Green", "Yellow", "Blue"]
+        playedCard = self.playerList[playerNum].playCard(cardNum)
+        if (playedCard.denom == 11):
+            ##print "Reverse code goes here"
+            self.direction *= -1
+        if (playedCard.denom == 12):
+            ##print "Draw 2 code goes here"
+            drawingPlayer = playerNum + self.direction
+            if (drawingPlayer == 4):
+                drawingPlayer = 0
+            if (drawingPlayer == -1):
+                drawingPlayer = 3
+            self.playerList[drawingPlayer].drawCards(self.deck.dealCards(2))
+        if (playedCard.denom == 13):
+            ##print "Wildcard code goes here"
+            colorPicker = randrange(0,3)
+            playedCard.color = colors[colorPicker]
+            print "Chosen color is: " + str(playedCard.color)
+        if (playedCard.denom == 14):
+            ##print "Wildcard +4 code goes here"
+            drawingPlayer = playerNum + self.direction
+            if (drawingPlayer == 4):
+                drawingPlayer = 0
+            if (drawingPlayer == -1):
+                drawingPlayer = 3
+            self.playerList[drawingPlayer].drawCards(self.deck.dealCards(4))
+            colorPicker = randrange(0,3)
+            playedCard.color = colors[colorPicker]
+            print "Chosen color is: " + str(playedCard.color)
+        ##self.currentPlayer += self.direction
+        ##if (self.currentPlayer == 4):
+            ##self.currentPlayer = 0
+        ##if (self.currentPlayer == -1):
+            ##self.currentPlayer = 3
+        self.pile.append(playedCard)
+
+    def getValidPlays(self, playerNum):
+        pileTop = self.pile[-1]
+        validCards = []
+        i = 0
+        for item in self.playerList[playerNum].hand:
+            if((item.denom == pileTop.denom) or (item.color == pileTop.color)):
+                validCards.append(i)
+            if(item.color == "Wild"):
+                validCards.append(i)
+            i += 1
+        return validCards
+
+    def takeTurn(self, playerNum):
+        ##print self.currentPlayer
+        if (self.deck.length() <= 5):
+            self.shufflePile()
+        ##print "Top card of pile is : " + str(self.pile[-1])
+        makePlay = True
+        validPlays = self.getValidPlays(playerNum)
+        if (len(validPlays) == 0):
+            print self.playerList[playerNum].name + " can't make a move and draws a card"
+            self.playerList[playerNum].drawCards(self.deck.dealCards(1))
+            validPlays = self.getValidPlays(playerNum)
+            if (len(validPlays) == 0):
+                print "No plays available for " + self.playerList[playerNum].name + ", passing turn"
+                ##self.currentPlayer += self.direction
+                ##if (self.currentPlayer == 4):
+                    ##self.currentPlayer = 0
+                ##if (self.currentPlayer == -1):
+                    ##self.currentPlayer = 3
+                makePlay = False
+        if (makePlay == True):
+            if (len(validPlays) == 1):
+                picker = 0
+            else:
+                picker = (randrange(1,len(validPlays)))
+                picker -= 1
+            ##print "Cards in hand: "
+            ##for item in self.playerList[playerNum].hand:
+                ##print item
+            ##print "picker "+ str(picker)
+            ##print "valid plays:"
+            ##for item in validPlays:
+                ##print item
+            print str(self.playerList[playerNum].name) + " plays: " + str(self.playerList[playerNum].hand[validPlays[picker]])
+            self.playerPlayCard(playerNum, validPlays[picker])
+            if (len(self.playerList[playerNum].hand) == 1):
+                print "UNO!"
+            if (len(self.playerList[playerNum].hand) == 0):
+                print "Game Over!"
+                self.gameOver = True
+                print "Winner: " + self.playerList[playerNum].name
+
+
+        self.currentPlayer = self.currentPlayer + self.direction
+        if (self.currentPlayer == 4):
+            self.currentPlayer = 0
+        if (self.currentPlayer == -1):
+            self.currentPlayer = 3
+        ##print self.currentPlayer
+
+    def shufflePile(self):
+        pileCard = self.pile.pop(-1)
+        for item in self.pile:
+            if (item.denom == "Card" or item.denom == "Card +4"):
+                item.color = "Wild"
+            self.deck.append(item)
+        self.pile = [pileCard]
+        self.deck.shuffle()
+
 def createTable():
     currentDeck = unoDeck()
     currentDeck.shuffle()
@@ -81,17 +199,21 @@ def createTable():
     theTable.createPlayer("Player 1")
     theTable.createPlayer("Player 2")
     theTable.createPlayer("Player 3")
-    theTable.createPlayer("Human Player")
+    theTable.createPlayer("Player 4")
     theTable.dealOpeners()
+    theTable.pile.append(theTable.deck.dealCards(1)[0])
+    while (theTable.pile[-1].color == "Wild"):
+        theTable.pile.append(theTable.deck.dealCards(1)[0])
     return theTable
 
 currentTable = createTable()
-for item in currentTable.playerList:
-    print item.name
-    for card in item.hand:
+print "Top card of pile is : " + str(currentTable.pile[-1])
+while (currentTable.gameOver == False):
+    currentTable.takeTurn(currentTable.currentPlayer)
+for player in currentTable.playerList:
+    print player.name
+    print len(player.hand)
+    for card in player.hand:
         print card
-    print "\n"
-
-
 
 
